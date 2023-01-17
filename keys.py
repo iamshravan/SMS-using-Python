791090351d3a36d7ccce699637c21c6501e6a053
iamshravan <86182557+iamshravan@users.noreply.github.com>
2023-01-17 21:40:37 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,4 @@
+account_sid= 'ACfb3469ef6a7dc2a7691cc220a5bcd646'
+auth_token = '751e2f84bd1a84a55d1dc98bd37af14e'
+twilio_number ='+15625737916'
+my_phone_number = '+918310024842'
---
+++ main.py
@@ -0,0 +1,8 @@
+from twilio.rest import Client
+import keys
+client = Client(keys.account_sid, keys.auth_token)
+message = client.messages.create(
+    body="Python Project exhibition",
+    from_=keys.twilio_number,
+    to=keys.my_phone_number
+)
